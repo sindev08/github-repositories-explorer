c591f54ed9d9b0ae5a5e19abdc4d4f59574449a4
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2025-03-10 11:02:15 +0700

    clean console.log

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -42,7 +42,6 @@ export default function Home() {
   }, [selectedUser, refetch]);
 
   const { resolvedTheme } = useTheme();
-  console.log(resolvedTheme);
 
   return (
     <div className="h-screen w-full relative">
